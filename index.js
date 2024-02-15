b864d1174d148e285c68838b025403be5afc217c
Oscar_Gutierrez <oscarfgutierrezo@gmail.com>
2024-02-14 19:52:50 -0500

    Add: post data con UserSchema

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
+// Importaciones
 const express = require("express");
+const mongoose = require("mongoose");
+const UserSchema = require("./models/User");
+
+// Conexion a base de datos Mongo
+mongoose.connect(
+  "mongodb+srv://oscarfgutierrezo:Fernando_71611556@cluster0.b92mxy5.mongodb.net/talentotech-users"
+);
+
+// Instancia Express
 const app = express();
 
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Manejo de rutas
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
 
-// Parametros desde url
 app.get("/name", (req, res) => {
   res.send(req.query.name);
 });
 
-// Parámetros desde body de la solicitur
 app.post("/user", (req, res) => {
-  const user = {
+  const user = new UserSchema({
     name: req.body.name,
     lastname: req.body.lastname,
-  };
+  });
+  user.save();
   res.json(user).status(201);
 });
 
+// Inicio de servidor, escuchando en el puerto indicado
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
